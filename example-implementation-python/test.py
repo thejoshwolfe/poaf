c43f5062efd7d3753e8ef8fdd15edf3abb2535e3
Josh Wolfe <thejoshwolfe@gmail.com>
2025-04-19 11:29:25 -0400

    test finds new tests location

--- example-implementation-python/test.py
+++ example-implementation-python/test.py
@@ -17,6 +17,10 @@ from common import (
     FILE_TYPE_SYMLINK,
 )
 
+this_dir = os.path.dirname(os.path.abspath(__file__))
+repo_dir = os.path.dirname(this_dir)
+test_dir = os.path.join(repo_dir, "tests")
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
@@ -27,7 +31,7 @@ def main():
     test_create()
 
 def test_from_data(verbose):
-    with open("test_data.json") as f:
+    with open(os.path.join(test_dir, "test_data.json")) as f:
         test_data = json.load(f)
 
     current_group = None
@@ -132,18 +136,18 @@ def test_create():
         with tempfile.NamedTemporaryFile() as f:
             archive_path = f.name
             cmd.extend(["--output", archive_path])
-            subprocess.run(cmd, check=True)
+            subprocess.run(cmd, cwd=this_dir, check=True)
 
             # List index
             cmd = ["./read.py", archive_path]
-            #subprocess.run(cmd, check=True)
-            lines = subprocess.run(cmd, stdout=subprocess.PIPE, check=True).stdout.decode("utf8").splitlines()
+            #subprocess.run(cmd, cwd=this_dir, check=True)
+            lines = subprocess.run(cmd, stdout=subprocess.PIPE, cwd=this_dir, check=True).stdout.decode("utf8").splitlines()
             assert lines == file_names
 
             # Extract streaming
             with tempfile.TemporaryDirectory() as d:
                 cmd = ["./read.py", archive_path, "--extract", d]
-                subprocess.run(cmd, check=True)
+                subprocess.run(cmd, cwd=this_dir, check=True)
                 assert_dir(d, file_name_args, file_names)
 
             # Extract random access
@@ -151,7 +155,7 @@ def test_create():
                 # Extract each item in individual calls.
                 for name in reversed(file_names):
                     cmd = ["./read.py", archive_path, "--extract", d, name]
-                    subprocess.run(cmd, check=True)
+                    subprocess.run(cmd, cwd=this_dir, check=True)
                 assert_dir(d, file_name_args, file_names)
 
 def assert_dir(d, file_name_args, file_names):
@@ -159,7 +163,7 @@ def assert_dir(d, file_name_args, file_names):
     assert set(found_files) == set(file_names)
     for arg, name in zip(file_name_args, file_names):
         source_path = arg.split("->")[0]
-        assert read_file(source_path) == read_file(os.path.join(d, name)), name
+        assert read_file(os.path.join(this_dir, source_path)) == read_file(os.path.join(d, name)), name
 
 def read_file(path):
     with open(path, "rb") as f:
